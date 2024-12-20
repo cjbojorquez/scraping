d59611622a16f9a5cd5a32538807038b696a622a
Cesar Joel Bojorquez Curtidor <cjbojorquez@tigo.com.gt>
2024-12-20 13:50:32 -0600

    prueba inicial scraping

---
+++ script.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url ='https://dockerlabs.es'
+respuesta = requests.get(url)
+
+if respuesta.status_code == 200:
+    soup= BeautifulSoup(respuesta.text,'html.parser')
+
+    maquinas = soup.find_all('div', onclick=True)
+
+    for maquina in maquinas:
+        onclick_text = maquina['onclick']
+        nombre_maquina = onclick_text.split("'")[1]
+        print(nombre_maquina)
+
+    else:
+        print(f'Hubo un error al hacer la peticion {respuesta.status_code}')
